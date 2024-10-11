bfc67c5e4b4a09341160411360c74c6be63dc343
wwb-cloud <15607026226@163.com>
2024-10-11 16:01:41 +0800

    Update structs2.rs

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -36,10 +36,10 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         let your_order =Order{
-             count:1,
-            ..order_template
+             count:1;
+            ..order_template;
         }
-        assert_eq!(your_order.name, "Hacker in Rust");
+        assert_eq!(your_order.name, "Bob");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
         assert_eq!(your_order.made_by_mobile, order_template.made_by_mobile);
